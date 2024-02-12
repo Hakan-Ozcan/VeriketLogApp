a6ffa5df0fcb6a11833bba414df51c1d5e3f09b5
RntService <opuqo@rentgo.com>
2024-02-12 10:48:23 +0300

    columns fixed

--- FormSec/Form1.cs
+++ FormSec/Form1.cs
@@ -18,19 +18,17 @@ namespace FormSec
         {
             try
             {
+                int count = 0;
+              
                 //_httpClient.BaseAddress = new Uri("http://localhost:5000/");
                 HttpResponseMessage response = await _httpClient.GetAsync("http://localhost:5000/VeriketApplicationTest");
                 var logs = await response.Content.ReadAsStringAsync();
-
+             
                 dataGridView1.Rows.Clear(); // Önceki verileri temizle
 
                 var logLines = logs.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-                dataGridView1.Columns.Add("Column1", "Tarih");
-                dataGridView1.Columns.Add("Column2", "Bilgisayar Adı");
-                dataGridView1.Columns.Add("Column3", "Windowsta Oturum Açan Kullanıcı Adı");
-                dataGridView1.Columns[0].Width = 200;
-                dataGridView1.Columns[1].Width = 200;
-                dataGridView1.Columns[2].Width = 200;
+
+              
                 foreach (var logLine in logLines)
                 {
                     var logParts = logLine.Split(',');
@@ -48,7 +46,12 @@ namespace FormSec
 
         private async void Form1_Load(object sender, EventArgs e)
         {
-           
+            dataGridView1.Columns.Add("Column1", "Tarih");
+            dataGridView1.Columns.Add("Column2", "Bilgisayar Adı");
+            dataGridView1.Columns.Add("Column3", "Windowsta Oturum Açan Kullanıcı Adı");
+            dataGridView1.Columns[0].Width = 200;
+            dataGridView1.Columns[1].Width = 200;
+            dataGridView1.Columns[2].Width = 200;
             await _httpClient.PostAsync("http://localhost:5000/VeriketApplicationTest", null);
           
         }
